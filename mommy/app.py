af794aaa53422c95e8ac0a726ae7fbfb317ae7c5
Felipe Alves <106632694+felipelvs@users.noreply.github.com>
2022-06-04 14:59:38 -0300

    :bug: Fix Flask app not running

--- mommy/app.py
+++ mommy/app.py
@@ -4,15 +4,15 @@ Prepares the application to run.
 If the file is opened for execution, it runs the application.
 """
 
-from flask import Flask
 
-from mommy.views import main_bp
+from flask import Flask
+from views import main_bp
 
 
 def create_app():
     """Creates and configures the Flask instance."""
     app = Flask(__name__)
-    app.config.from_object("mommy.config.DevelopmentConfig")
+    app.config.from_object("config.DevelopmentConfig")
 
     app.register_blueprint(main_bp)
 
