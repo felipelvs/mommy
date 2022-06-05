685185cc065ee151d45b94f038a821504809695d
Felipe Alves <106632694+felipelvs@users.noreply.github.com>
2022-06-05 09:57:14 -0300

    :card_file_box: Implement database directories

--- mommy/config.py
+++ mommy/config.py
@@ -12,6 +12,9 @@ class ProductionConfig(Config):
 
     SECRET_KEY = ""
 
+    SQLALCHEMY_DATABASE_URI = ""
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     """Configuration used for development."""
@@ -19,6 +22,9 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
     SECRET_KEY = "c0cf0dc51db2a9d5eb29e79e28f3b72f"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database-dev.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
@@ -28,3 +34,6 @@ class TestingConfig(Config):
     TESTING = True
 
     SECRET_KEY = "18f59a8d425e7c3e0f17755e1f274c3c"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///../tests/database-tests.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
